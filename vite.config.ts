060ef97d6c7e67df650d7e68d282b24cb62fd0e9
Nils Wellekens <nils.wellekens@student.hogent.be>
2026-04-30 01:44:17 +0200

    dsf

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/job-board-appp/",
   plugins: [
     react(),
     // Removed splitVendorChunkPlugin() - we're using manual chunks instead
